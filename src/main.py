d659472afe21514bb55808a495e4bf876dacdd6a
Kyle Brandon <kyle@brandfam.net>
2024-06-18 19:19:43 -0500

    save time and temp to file

--- src/main.py
+++ src/main.py
@@ -1,5 +1,19 @@
+from temp import read_temp
+import time
+import datetime
+
+
 def main():
-    print("this is a test")
+    with open("./temperatures.txt", "w") as f:
+        while True:
+            temp_f = read_temp()
+            current_time = datetime.datetime.now()
+            temperature = f"{current_time} - {temp_f}\n"
+            f.write(temperature)
+            print(temperature)
+
+            f.flush()
+            time.sleep(1)
 
 
 main()
